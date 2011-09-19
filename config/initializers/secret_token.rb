566a280ec0f74b381fc756095caa4e88b003a52a
Greg Caplan <gdcaplan@gmail.com>
2011-09-19 17:34:47 -0500

    update lockfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b0aa54ce40313b9076a35f11100192afcb4334c743247f0ada43af336b82eff124df7b121310fb42ed031ec16aae1ae16aa8b2e37d3f53028026fdc29d7b898d'
